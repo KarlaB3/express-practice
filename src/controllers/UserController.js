26a60f35105b9bcca4a3b818b7b7a6eff96d5f91
KarlaB3 <k.bucoy@gmail.com>
2023-06-11 17:38:32 +1000

    refactor: moved signup logic to middleware

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,16 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.post('/signup', (request, response) => {
+const { bodyHasEmail, bodyHasPassword, emailIsValid } = require('../middleware/UserMiddleware')
 
-    if (request.body.email && 
-        request.body.email.includes("@")
-        &&
-        request.body.password) {
+router.post('/signup', 
+    // check for email middleware
+    bodyHasEmail,
+
+    // check if email is valid middleware
+    emailIsValid,
+
+    // check for password middleware
+    bodyHasPassword,
+
+
+    (request, response) => {
+        // if we reach the request, response block of route, assume there are no errors
         response.json({message:"Sign up success!"});
-    } else {
-        response.status(400).json({message:"Sign up failure!"})
-    }
+
+    // if (request.body.email && 
+    //     request.body.email.includes("@")
+    //     &&
+    //     request.body.password) {
+    //     response.json({message:"Sign up success!"});
+    // } else {
+    //     response.status(400).json({message:"Sign up failure!"})
+    // }
 })
 
 module.exports = router;
